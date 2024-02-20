886f096bfb87afa27f36b1582d27cc82b9069382
Cristian Betancur <34930340+cristiandbetancurg@users.noreply.github.com>
2024-02-19 21:38:26 -0400

    Update timeline.jsx
    Estados del item y elemento draggables

--- timeline.jsx
+++ timeline.jsx
@@ -1,2 +1,13 @@
 import React, { useState } from 'react';
-import { Timeline, Item } from 'react-visjs';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const App = () => {
+  const [items, setItems] = useState([
+    { id: 1, content: '0 NGOLD', achieved: false },
+    { id: 2, content: '250 NGOLD', achieved: false },
+    { id: 3, content: '500 NGOLD', achieved: false },
+    { id: 4, content: '750 NGOLD', achieved: false },
+    { id: 5, content: '1000 NGOLD', achieved: false },
+  ]);
+
+}
