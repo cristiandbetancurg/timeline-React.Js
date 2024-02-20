13a0464a0b6857cb2b4dcbe62218c7ccef8651ae
Cristian Betancur <34930340+cristiandbetancurg@users.noreply.github.com>
2024-02-19 22:33:30 -0400

    Update timeline.jsx
    desarrollo de función handleDragEnd que se va a ejecutar a medida que se arrastre un elemento

--- timeline.jsx
+++ timeline.jsx
@@ -9,5 +9,16 @@ const App = () => {
     { id: 4, content: '750 NGOLD', achieved: false },
     { id: 5, content: '1000 NGOLD', achieved: false },
   ]);
+const handleDragEnd = (result) => {
+    const { destination, source } = result;
 
+    // Si y solo si el elemento se ha dejado en un área válida determinada
+    if (destination) {
+      const newItems = [...items];
+      const [reorderedItem] = newItems.splice(source.index, 1);
+      newItems.splice(destination.index, 0, reorderedItem);
+
+      setItems(newItems);
+    }
+  };
 }
